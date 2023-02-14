76fb9dc773e0a1a0917a7d9022208e0f79f88773
Ilya Anikeyenka <20prorok02@gmail.com>
2023-02-14 12:43:30 +0300

    Solution

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)) {
+            if ("aeiouAEIOU".indexOf(character) != -1) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if (month >= 13 || month < 1) {
+            System.out.println("wrong number!");
+        } else if (month == 12 || month == 1 || month == 2) {
+            System.out.println("Winter");
+        } else if (month >= 9) {
+            System.out.println("Autumn");
+        } else if (month >= 6) {
+            System.out.println("Summer");
+        } else {
+            System.out.println("Spring");
+        }
+
 
     }
 }
